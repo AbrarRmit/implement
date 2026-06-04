3a505ef4e523340c4cd169e7078472763bad0833
Vulchaestus <trudoxhighwind@gmail.com>
2026-06-04 22:42:38 +1000

    Fixed electric bus driver needs Heavy or PublicTransport LicenseType

--- src/main/java/com/assignment/helper/BusAndDriver.java
+++ src/main/java/com/assignment/helper/BusAndDriver.java
@@ -5,27 +5,30 @@ import com.assignment.model.Driver;
 
 public class BusAndDriver {
 
-    //Reject bus driver older than 50 years old to drive bus with more than 49 people
-    public boolean driverAgeRestriction(Driver d,Bus b){
+   // Reject bus driver older than 50 years old to drive bus with more than 49
+   // people
+   public boolean driverAgeRestriction(Driver d, Bus b) {
 
-        if(b.getCapacity() >= 50 && d.getAge() > 50 ){
-            return false;
-        }
-        return true;
-    }
-    //Reject drivers with less than 5 years of experience from electric bus
-    public boolean electricBusRestriction(Driver d,Bus b){
-        if(b.getFuelType().equals("Electricity") && d.getExperienceYears() < 5){
-            return false;
-        }
-        return true;
-    }
- 
-    public boolean driverLicenceRestriction(Driver d,Bus b){
-        if((b.getFuelType().equals("Electric") || b.getFuelType().equals("Hybrid")) && (!d.getLicenseType().equals("Heavy") && !d.getLicenseType().equals("PublicTransport"))){
-            return false;
-        }
-        return true;
-    }
+      if (b.getCapacity() >= 50 && d.getAge() > 50) {
+         return false;
+      }
+      return true;
+   }
+
+   // Reject drivers with less than 5 years of experience from electric bus
+   public boolean electricBusRestriction(Driver d, Bus b) {
+      if (b.getFuelType().equals("Electricity") && d.getExperienceYears() < 5) {
+         return false;
+      }
+      return true;
+   }
+
+   // Electric and hybrid buses require a Heavy or PublicTransport licence
+   public boolean driverLicenceRestriction(Driver d, Bus b) {
+      if ((b.getFuelType().equals("Electricity") || b.getFuelType().equals("Hybrid"))
+            && (!d.getLicenseType().equals("Heavy") && !d.getLicenseType().equals("PublicTransport"))) {
+         return false;
+      }
+      return true;
+   }
 }
-
